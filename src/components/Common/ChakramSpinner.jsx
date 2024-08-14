291d10d1b1cd5a9ddd13925f15f069859ce653bf
ompateldeveloper <ompateldeveloper@gmail.com>
2024-08-14 13:59:59 +0530

    bug fixes , ui fixes

--- src/components/Common/ChakramSpinner.jsx
+++ src/components/Common/ChakramSpinner.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-
+import React from "react";
 
 const ChakramSpinner = () => (
-    <div className="fixed inset-0 flex items-center justify-center  z-50">
-         <img src="/Chakram2-nobg.png" alt="Lord Venkateswara Chakram" className="animate-spin rounded-full h-30 w-28  " />
-  </div>
+    <div className="fixed inset-0 flex items-center h-screen justify-center bg-zinc-100 bg-blur-sm z-50 ">
+        <img src="/Chakram2-nobg.png" alt="Lord Venkateswara Chakram" className="animate-ping origin-[50%_55%] rounded-full h-30 w-28  " />
+    </div>
 );
 
 export default ChakramSpinner;
